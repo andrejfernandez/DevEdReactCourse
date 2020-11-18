645cf5337fead14215e8fb9b05a8a888eabe2fe7
Fernandez <andre.fernandez.ext@siemens.com>
2020-11-18 11:41:33 -0500

    Added to music player

--- Chapter-03/react-music-player/src/App.js
+++ Chapter-03/react-music-player/src/App.js
@@ -21,6 +21,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
 
@@ -30,7 +31,17 @@ function App() {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
     //console.log(e.target.duration);
-    setSongInfo({ ...songInfo, currentTime, duration });
+    //Calculate Percentage
+    const roundedCurrent = Math.round(currentTime);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    console.log(animation + "%");
+    setSongInfo({
+      ...songInfo,
+      currentTime,
+      duration,
+      animationPercentage: animation,
+    });
   };
 
   return (
