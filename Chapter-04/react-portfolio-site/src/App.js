b937d64c1367c3f63e48ac21309709ba15b5f0db
Andre <andrefernandez250@gmail.com>
2020-12-22 22:04:25 -0600

    Added React Router

--- Chapter-04/react-portfolio-site/src/App.js
+++ Chapter-04/react-portfolio-site/src/App.js
@@ -1,12 +1,28 @@
 import AboutUs from "./Pages/AboutUs.js";
+import OurWork from "./Pages/OurWork.js";
+import ContactUs from "./Pages/ContactUs.js";
+import Nav from "./Components/Nav";
 // Global Style
 import GlobalStyle from "./Components/GlobalStyle";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
