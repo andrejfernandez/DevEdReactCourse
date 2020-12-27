424a68b35192755b677d224b40a5a4ce92d98ede
Andre <andrefernandez250@gmail.com>
2020-12-27 14:46:32 -0600

    Animated AboutSection

--- Chapter-04/react-portfolio-site/src/Components/AboutSection.js
+++ Chapter-04/react-portfolio-site/src/Components/AboutSection.js
@@ -4,6 +4,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../Styles";
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -11,27 +12,27 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnim}>come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography/videography jobs such as Lorem ipsum
           dolor sit amet consectetur, adipisicing elit. Quod debitis ipsa
           mollitia natus impedit, nihil culpa cumque consequatur voluptatum ad
           quas quae maiores odit saepe perspiciatis? Explicabo ipsum beatae non?
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Dude with camera" />
+        <motion.img src={home1} alt="Dude with camera" variants={photoAnim} />
       </Image>
     </About>
   );
